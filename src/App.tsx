ddc651b8f0ccaefc9e55628d013079cad58a27b2
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-22 15:25:38 +0300

    Map pages to create routes for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import pages from "./pages/content/pages"
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <>
+        <div>
+            <Routes>
+                {pages.map(page => {
+                    const {path, Component, content} = page;
 
-        </>
+                    return (
+                        <Route
+                            path={path}
+                            element={<Component content={content}/>}
+                        />
+                    )
+                })}
+            </Routes>
+
+        </div>
     )
 }
 
