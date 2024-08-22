cfcd6c21bf487fc06ed1b954ea424f8b7e0761a5
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-22 15:25:08 +0300

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+import Content from "./content/Content.ts";
+
+const Home: FC<Content> = ({content}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Home;
