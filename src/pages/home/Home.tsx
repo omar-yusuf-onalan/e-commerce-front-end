bec7f5b4e070681cce8c089ac5a0900dd6ffdfe3
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-27 19:52:35 +0300

    Add hero

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,15 +3,19 @@ import Content from "../Content.ts";
 import {HeaderContent} from "./content/header.ts";
 import Header from "../../components/Header.tsx";
 
+import Hero from "../../components/Hero.tsx";
+import {HeroObject} from "./content/hero.ts";
+
 export interface HomeProps extends Content {
     header: HeaderContent;
+    hero: HeroObject[];
 }
 
-const Home: FC<HomeProps> = ({header}) => {
-
+const Home: FC<HomeProps> = ({header, hero}) => {
     return (
         <div>
             <Header {...header}/>
+            <Hero hero={hero}/>
         </div>
     )
 }
