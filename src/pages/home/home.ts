b0a2df3c66fc07d01d1c66ceb4c4b4c4605b3595
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-27 19:52:26 +0300

    Add hero

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,15 @@
 import Home from "./Home.tsx";
 import Page from "../Page.ts";
 import header from "./content/header.ts";
+import hero from "./content/hero.ts";
 
 const home: Page =  {
     path: "/",
     // @ts-ignore
     Component: Home,
     content: {
-        header: header
+        header: header,
+        hero: hero
     }
 }
 
