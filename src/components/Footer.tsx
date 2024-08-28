1ada15629a155d57b27aadc8a54fcd8d35e2d60f
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-28 10:03:11 +0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import {FC} from "react";
+import {FooterContent} from "../pages/home/content/footer.ts";
+
+const Footer: FC<FooterContent> = ({title, description, contactUsButton, links, madeBy, socialMedia}) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Footer;
