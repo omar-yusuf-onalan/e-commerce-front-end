5d9cdfee4764f6e5ef93bd6d4cf0ff1aa6095dfc
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-28 12:53:12 +0300

    Add footer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import Home from "./Home.tsx";
 import Page from "../Page.ts";
 import header from "./content/header.ts";
 import hero from "./content/hero.ts";
+import footer from "./content/footer.ts";
 
 const home: Page =  {
     path: "/",
@@ -9,7 +10,8 @@ const home: Page =  {
     Component: Home,
     content: {
         header: header,
-        hero: hero
+        hero: hero,
+        footer: footer,
     }
 }
 
