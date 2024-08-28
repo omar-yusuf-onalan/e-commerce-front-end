f66bbbaa96612e074d1a0f6f7802e40d0c03fb09
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-28 10:03:19 +0300

    Add Footer component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,20 +2,23 @@ import {FC} from "react";
 import Content from "../Content.ts";
 import {HeaderContent} from "./content/header.ts";
 import Header from "../../components/Header.tsx";
-
 import Hero from "../../components/Hero.tsx";
 import {HeroObject} from "./content/hero.ts";
+import {FooterContent} from "./content/footer.ts";
+import Footer from "../../components/Footer.tsx";
 
 export interface HomeProps extends Content {
     header: HeaderContent;
     hero: HeroObject[];
+    footer: FooterContent;
 }
 
-const Home: FC<HomeProps> = ({header, hero}) => {
+const Home: FC<HomeProps> = ({header, hero, footer}) => {
     return (
         <div>
             <Header {...header}/>
             <Hero hero={hero}/>
+            <Footer {...footer}/>
         </div>
     )
 }
