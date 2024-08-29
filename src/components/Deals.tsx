6274820a71f1a84896c714dc83b19cad8df822d1
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-08-29 14:43:36 +0300

    Create Deals.tsx

---
+++ src/components/Deals.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+import {DealsContent} from "../pages/home/content/deals.ts";
+
+const Deals: FC<DealsContent> = ({content}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Deals;
